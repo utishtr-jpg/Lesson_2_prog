814f53966c6afde439d4d290bb407ea56f2c8812
utishtr-jpg <utishtr@gmail.com>
2025-12-04 16:43:57 -0800

    birthday_date.py

---
+++ birthday_date.py
@@ -0,0 +1,11 @@
+print("This program is used to enter people's birthdays!")
+ubday= "Oct 8 2014 10/08/2014"
+rbday= "Oct 8 1977 10/08/1977"
+tbday= "Jun 26 2007 6/26/2007"
+sbday="Dec 18 1980 12/18/1980"
+pbday="Dec 5 1950 12/5/1950"
+print("Utisht's birthday is", ubday)
+print("Tarun's birthday is", tbday)
+print("Ram's birthday is", rbday)
+print("Suba's birthday is", sbday)
+print("Paati's birthday is", pbday)
