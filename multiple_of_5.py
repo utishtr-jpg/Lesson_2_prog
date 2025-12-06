6428c99ac7613c03cc50468e7fd575a9da931563
utishtr-jpg <utishtr@gmail.com>
2025-12-05 20:12:25 -0800

    multiple_of_5

---
+++ multiple_of_5.py
@@ -0,0 +1,6 @@
+print("This program is to calculate if a number is a multiple of 5.")
+num=int(input("Please enter a number!"))
+if num % 5 == 0:
+    print("Your number is a multiple of 5! ")
+else:
+    print("Your number is not a multiple of 5.")
