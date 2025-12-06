0c753f50044a9bd51af871288f778ad5bafc4e91
utishtr-jpg <utishtr@gmail.com>
2025-12-06 12:35:09 -0800

    1_through_7

---
+++ 1_through_7.py
@@ -0,0 +1,26 @@
+print("This program will check if a number is a number from 1 through 7")
+num=int(input("Please enter a number!"))
+if num > 7:
+    print("Your number is not valid.")
+if num < 1:
+    print("Your number is not valid.")
+if num==1:
+    print("It is Sunday")
+
+if num==2:
+    print("It is Monday")
+
+if num==3:
+    print("It is Tuesday")
+
+if num==4:
+    print("It is Wednesday")
+
+if num==5:
+    print("It is Thursday")
+
+if num==6:
+    print("It is Friday")
+
+if num==7:
+    print("It is Saturday")
